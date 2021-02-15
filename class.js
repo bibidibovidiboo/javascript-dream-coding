3ae23f470ae47c6b9738811a9d84daf15a1bec0d
Bomi Lee <bibidibovidiboo@gmail.com>
2021-02-15 19:44:07 +0900

    Update class.js

--- class.js
+++ class.js
@@ -1,11 +1,3 @@
-'use strict'
-// Object-oriendted programming
-// class: template
-// object: instance of a class
-// JavaScript classes
-//  - introduced in ES6
-//  - syntactical sugar over prototype-based inheritance
-
 // 1. Class declarations
 class Person {
   // constructor
