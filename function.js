cdfa18a7af3a07820d6958f971c57b8f498c7b13
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2021-03-10 15:36:10 +0900

    Update js.files

--- function.js
+++ function.js
@@ -7,69 +7,74 @@
 // function name(param1, param2) { body... return; }
 // one function === one thing
 // naming: doSomething, command, verb
-// e.g. createCardAndPoint -> createCard, createPoint
+// e.g. createCardAndPoint -> createCard, createPoint => 두개로 나눌 순 없는지
 // function is object in JS
 function printHello() {
-  console.log('Hello')
+  console.log('Hello');
 }
-printHello()
+printHello();
 
 function log(message) {
-  console.log(message)
+  console.log(message);
 }
-log('Hello@')
-log(1234)
+log('Hello@');
+log(1234); // 타입이 없기 때문에 숫자를 전달할 수 있음
+
+function log(message) {
+  console.log(message);
+}
+log('Hello');
 
 // 2. Parameters
 // premitive parameters: passed by value
 // object parameters: passed by reference
 function changeName(obj) {
-  obj.name = 'coder'
+  obj.name = 'coder';
 }
-const ellie = { name: 'ellie' }
-changeName(ellie)
-console.log(ellie)
+const ellie = { name: 'ellie' };
+changeName(ellie);
+console.log(ellie);
 
 // 3. Default parameters (added in ES6)
 function showMessage(message, from = 'unknown') {
-  console.log(`${message} by ${from}`)
+  console.log(`${message} by ${from}`);
 }
-showMessage('Hi!')
+showMessage('Hi!');
 
-// 4. Rest parameters (added in ES6)
+// 4. Rest parameters (added in ES6) 배열형태로 전달
 function printAll(...args) {
   for (let i = 0; i < args.length; i++) {
-    console.log(args[i])
+    console.log(args[i]);
   }
 
   for (const arg of args) {
-    console.log(arg)
+    console.log(arg);
   }
 
-  args.forEach(arg => console.log(arg))
+  args.forEach(arg => console.log(arg));
 }
-printAll('dream', 'coding', 'ellie')
+printAll('dream', 'coding', 'ellie');
 
 // 5. Local scope
-let globalMessage = 'global' // global variable
+let globalMessage = 'global'; // global variable
 function printMessage() {
-  let message = 'hello'
-  console.log(message) // local variable
-  console.log(globalMessage)
+  let message = 'hello';
+  console.log(message); // local variable 지역변수
+  console.log(globalMessage);
   function printAnother() {
-    console.log(message)
-    let childMessage = 'hello'
+    console.log(message);
+    let childMessage = 'hello';
   }
   // console.log(childMessage); //error
 }
-printMessage()
+printMessage();
 
 // 6. Return a value
 function sum(a, b) {
-  return a + b
+  return a + b;
 }
-const result = sum(1, 2) // 3
-console.log(`sum: ${sum(1, 2)}`)
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
 
 // 7. Early return, early exit
 // bad
@@ -82,7 +87,7 @@ function upgradeUser(user) {
 // good
 function upgradeUser(user) {
   if (user.point <= 10) {
-    return
+    return;
   }
   // long upgrade logic...
 }
@@ -98,35 +103,35 @@ function upgradeUser(user) {
 // a function expression is created when the execution reaches it.
 const print = function () {
   // anonymous function
-  console.log('print')
-}
-print()
-const printAgain = print
-printAgain()
-const sumAgain = sum
-console.log(sumAgain(1, 3))
+  console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
 
 // 2. Callback function using function expression
 function randomQuiz(answer, printYes, printNo) {
   if (answer === 'love you') {
-    printYes()
+    printYes();
   } else {
-    printNo()
+    printNo();
   }
 }
 // anonymous function
 const printYes = function () {
-  console.log('yes!')
-}
+  console.log('yes!');
+};
 
 // named function
 // better debugging in debugger's stack traces
 // recursions
 const printNo = function print() {
-  console.log('no!')
-}
-randomQuiz('wrong', printYes, printNo)
-randomQuiz('love you', printYes, printNo)
+  console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
 
 // Arrow function
 // always anonymous
@@ -134,17 +139,18 @@ randomQuiz('love you', printYes, printNo)
 //   console.log('simplePrint!');
 // };
 
-const simplePrint = () => console.log('simplePrint!')
-const add = (a, b) => a + b
+const simplePrint = () => console.log('simplePrint!');
+const add = (a, b) => a + b;
 const simpleMultiply = (a, b) => {
   // do something more
-  return a * b
-}
+  return a * b;
+};
 
 // IIFE: Immediately Invoked Function Expression
-;(function hello() {
-  console.log('IIFE')
-})()
+// 최근에는 잘 쓰이진 않음
+(function hello() {
+  console.log('IIFE');
+})();
 
 // Fun quiz time❤
 // function calculate(command, a, b)
@@ -153,17 +159,17 @@ const simpleMultiply = (a, b) => {
 function calculate(command, a, b) {
   switch (command) {
     case 'add':
-      return a + b
+      return a + b;
     case 'substract':
-      return a - b
+      return a - b;
     case 'divide':
-      return a / b
+      return a / b;
     case 'multiply':
-      return a * b
+      return a * b;
     case 'remainder':
-      return a % b
+      return a % b;
     default:
-      throw Error('unkonwn command')
+      throw Error('unkonwn command');
   }
 }
-console.log(calculate('add', 2, 3))
+console.log(calculate('add', 2, 3));
