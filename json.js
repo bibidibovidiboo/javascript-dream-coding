f3d2a331db04da1d3944a18a85bdf839c13a178c
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2021-03-17 18:36:38 +0900

    Update json.js

--- json.js
+++ json.js
@@ -1,50 +1,54 @@
-'use strict'
+'use strict';
 
 // JSON
 // JavaScript Object Notation
 
-// 1. Object to JSON
+// 1. Object to JSON 오브젝트를 제이슨으로
 // stringfy(obj)
-let json = JSON.stringify(true)
-console.log(json)
 
-json = JSON.stringify(['apple', 'banana'])
-console.log(json)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
 
 const rabbit = {
   name: 'tori',
   color: 'white',
   size: null,
   birthDate: new Date(),
+  // jump function
   jump: function () {
-    console.log(`${this.name} can jump!`)
-  }
-}
+    console.log(`${this.name} can jump!`);
+  },
+};
 
-json = JSON.stringify(rabbit)
-console.log(json)
+json = JSON.stringify(rabbit);
+console.log(json);
 
-json = JSON.stringify(rabbit, ['name', 'color', 'size'])
-console.log(json)
+json = JSON.stringify(rabbit, ['name', 'color', 'size']);
+console.log(json);
 
 json = JSON.stringify(rabbit, (key, value) => {
-  console.log(`key: ${key}, value: ${value}`)
-  return key === 'name' ? 'ellie' : value
-})
-console.log(json)
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'name' ? 'ellie' : value;
+});
+console.log(json);
 
-// 2. JSON to Object
+// 2. JSON to Object 제이슨을 오브젝트로
 // parse(json)
-console.clear()
-json = JSON.stringify(rabbit)
-console.log(json)
+// stringify => api
+
+console.clear();
+json = JSON.stringify(rabbit);
+console.log(json);
 const obj = JSON.parse(json, (key, value) => {
-  console.log(`key: ${key}, value: ${value}`)
-  return key === 'birthDate' ? new Date(value) : value
-})
-console.log(obj)
-rabbit.jump()
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
 // obj.jump();
 
-console.log(rabbit.birthDate.getDate())
-console.log(obj.birthDate.getDate())
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
